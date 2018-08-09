d3ce83d5df4d09335ba9a49dc5df270dfcd3c4a0
Nikolay Shitov <ns@devtodev.com>
2018-08-09 17:16:53 +0300

    fir working with env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Run sshd"
 /usr/sbin/sshd
 
 echo "Detect ip and forward ports to outside interface via socat"
-ip=$(ifconfig  | grep 'inet '| grep -v '127.0.0.1' | cut -d' ' -f 10 | awk '{ print $ANDROID_EMULATOR_API_VERSION_FOR_START}')
+ip=$(ifconfig  | grep 'inet '| grep -v '127.0.0.1' | cut -d' ' -f 10 | awk '{ print $1 }')
 echo "IP is: [${ip}]"
 echo "running socat port 5037"
 socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
@@ -19,20 +19,20 @@ socat tcp-listen:443,bind=$ip,fork tcp:127.0.0.1:443 &
 
 echo "Install/update sysimage and platform for [${ANDROID_EMULATOR_API_VERSION_FOR_START}]"
 case "${ANDROID_EMULATOR_API_VERSION_FOR_START}" in
-	${!API14}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM14} ${API14};;
-	${!API15}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM15} ${API15};;
-	${!API16}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM16} ${API16};;
-	${!API17}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM17} ${API17};;
-	${!API18}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM18} ${API18};;
-	${!API19}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM19} ${API19};;
-	${!API21}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM21} ${API21};;
-	${!API22}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM22} ${API22};;
-	${!API23}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM23} ${API23};;
-	${!API24}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM24} ${API24};;
-	${!API25}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM25} ${API25};;
-	${!API26}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM26} ${API26};;
-	${!API27}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM27} ${API27};;
-	${!API28}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM28} ${API28};;
+	${!API14@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM14} ${API14};;
+	${!API15@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM15} ${API15};;
+	${!API16@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM16} ${API16};;
+	${!API17@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM17} ${API17};;
+	${!API18@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM18} ${API18};;
+	${!API19@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM19} ${API19};;
+	${!API21@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM21} ${API21};;
+	${!API22@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM22} ${API22};;
+	${!API23@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM23} ${API23};;
+	${!API24@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM24} ${API24};;
+	${!API25@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM25} ${API25};;
+	${!API26@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM26} ${API26};;
+	${!API27@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM27} ${API27};;
+	${!API28@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM28} ${API28};;
 	*) 
 		echo "UNSUPPORTED API VERSION [${ANDROID_EMULATOR_API_VERSION_FOR_START}]"
 		exit 1
