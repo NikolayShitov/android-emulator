83451c41e9d9427df24028d90a9baaf848e834f2
Nikolay Shitov <ns@devtodev.com>
2018-08-10 22:26:34 +0300

    optimize install and update sdk packages add dbg output about fs space usage

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,22 +25,28 @@ socat tcp-listen:80,bind=$ip,fork tcp:127.0.0.1:80 &
 echo "running socat port 443"
 socat tcp-listen:443,bind=$ip,fork tcp:127.0.0.1:443 &
 
+echo "See free space:"
+df
+
+echo "Update installed sdk packages"
+${ANDOIRD_BIN}/sdkmanager --update
+
 echo "Install/update sysimage and platform for [${ANDROID_EMULATOR_API_VERSION_FOR_START}]"
 case "${ANDROID_EMULATOR_API_VERSION_FOR_START}" in
-	${!API14@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM14} ${API14};;
-	${!API15@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM15} ${API15};;
-	${!API16@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM16} ${API16};;
-	${!API17@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM17} ${API17};;
-	${!API18@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM18} ${API18};;
-	${!API19@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM19} ${API19};;
-	${!API21@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM21} ${API21};;
-	${!API22@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM22} ${API22};;
-	${!API23@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM23} ${API23};;
-	${!API24@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM24} ${API24};;
-	${!API25@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM25} ${API25};;
-	${!API26@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM26} ${API26};;
-	${!API27@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM27} ${API27};;
-	${!API28@}) ${ANDOIRD_BIN}/sdkmanager ${PLATFORM28} ${API28};;
+	${!API14@}) update ${PLATFORM14} ${API14};;
+	${!API15@}) update ${PLATFORM15} ${API15};;
+	${!API16@}) update ${PLATFORM16} ${API16};;
+	${!API17@}) update ${PLATFORM17} ${API17};;
+	${!API18@}) update ${PLATFORM18} ${API18};;
+	${!API19@}) update ${PLATFORM19} ${API19};;
+	${!API21@}) update ${PLATFORM21} ${API21};;
+	${!API22@}) update ${PLATFORM22} ${API22};;
+	${!API23@}) update ${PLATFORM23} ${API23};;
+	${!API24@}) update ${PLATFORM24} ${API24};;
+	${!API25@}) update ${PLATFORM25} ${API25};;
+	${!API26@}) update ${PLATFORM26} ${API26};;
+	${!API27@}) update ${PLATFORM27} ${API27};;
+	${!API28@}) update ${PLATFORM28} ${API28};;
 	*) 
 		echo "UNSUPPORTED API VERSION [${ANDROID_EMULATOR_API_VERSION_FOR_START}]"
 		exit 1
@@ -55,3 +61,23 @@ ${ANDOIRD_BIN}/avdmanager -v create avd \
 echo "Running emulator for [$ANDROID_EMULATOR_API_VERSION_FOR_START]"
 echo ${ANDROID_EMU}/emulator \
 	-avd ${ANDROID_EMULATOR_API_VERSION_FOR_START} -no-boot-anim -noaudio -no-window -gpu off -verbose -qemu -vnc :2 -enable-kvm
+	
+function update() {
+	res=${${ANDOIRD_BIN}/sdkmanager --list | sed -e '/Available Packages/q' | tail -n +4 | cut -d'|' -f 1 | grep "^\s*$1\s*$"}
+	if [ $? -eq 0 ]
+	then
+		echo "package already installed and updated: [$res]"
+	else
+		echo "install package: [$res]"
+		${ANDOIRD_BIN}/sdkmanager $1
+	fi
+	
+	res=${${ANDOIRD_BIN}/sdkmanager --list | sed -e '/Available Packages/q' | tail -n +4 | cut -d'|' -f 1 | grep "^\s*$2\s*$"}
+	if [ $? -eq 0 ]
+	then
+		echo "package already installed and updated: [$res]"
+	else
+		echo "install package: [$res]"
+		${ANDOIRD_BIN}/sdkmanager $2
+	fi
+}
